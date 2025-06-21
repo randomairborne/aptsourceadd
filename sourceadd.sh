71476f6ec5a37ac70dfcf8ad10197d0e8d9d3822
valkyrie_pilot <valk@randomairborne.dev>
2025-06-21 14:01:36 -0600

    use main

--- sourceadd.sh
+++ sourceadd.sh
@@ -8,5 +8,5 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://apt.valk.sh/$1/pgp-key.public -o /etc/apt/keyrings/valkapt-$1.asc
 sudo chmod a+r /etc/apt/keyrings/valkapt-$1.asc
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/valkapt-$1.asc] https://apt.valk.sh/$1 stable" | sudo tee /etc/apt/sources.list.d/valkapt-$1.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/valkapt-$1.asc] https://apt.valk.sh/$1 stable main" | sudo tee /etc/apt/sources.list.d/valkapt-$1.list > /dev/null
 sudo apt-get update
